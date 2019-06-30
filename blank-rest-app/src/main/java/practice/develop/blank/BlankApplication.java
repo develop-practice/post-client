a8ee5b5b320106947ca1e660167309d0c6ea5958
develop-practice <itou_ma@tdc.co.jp>
2019-06-30 11:19:45 +0900

    make-client
    getとpostのclientを作成

--- blank-rest-app/src/main/java/practice/develop/blank/BlankApplication.java
+++ blank-rest-app/src/main/java/practice/develop/blank/BlankApplication.java
@@ -1,13 +1,34 @@
 package practice.develop.blank;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+import practice.develop.blank.dto.SampleRequestBody;
+import practice.develop.blank.dto.SampleResponseBody;
+
 public class BlankApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlankApplication.class, args);
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		SampleRequestBody req = new SampleRequestBody();
+		req.setId("str");
+		req.setPlusNum(5);
+
+		// getの場合
+		String getParam = restTemplate.getForObject("http://127.0.0.1:8080/get-server?stringParam=str", String.class);
+		System.out.println("getParam : " + getParam);
+		System.out.println("---------------------------------------------------------------------------");
+
+		// postの場合
+		String postUrl = "http://127.0.0.1:8080/post-server";
+		ResponseEntity<SampleResponseBody> res = restTemplate.postForEntity(postUrl, req, SampleResponseBody.class);
+
+		System.out.println("id : " + res.getBody().getId());
+		System.out.println("num : " + res.getBody().getResultNum());
+
 	}
 
 }
---
+++ blank-rest-app/src/main/java/practice/develop/blank/dto/SampleRequestBody.java
@@ -0,0 +1,26 @@
+package practice.develop.blank.dto;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SampleRequestBody {
+
+	/** ID */
+	@NotBlank
+	@Size(max = 5)
+	private String id;
+
+	/** 数値 */
+	@Min(0)
+	@Max(100)
+	private int plusNum;
+
+}
